ceb429a0e308b21e5604aef25d94edb42c2ec5d1
User <Willspri@gmail.com>
2024-01-21 09:54:59 +1300

    added

---
+++ main.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.getElementById("lightButton");
+    var audio = new Audio("button-sound.mp3"); 
+
+    button.addEventListener("click", function () {
+        
+        button.style.backgroundColor = "#FFFF00"; 
+
+        
+        audio.play();
+
+        
+        setTimeout(function () {
+            button.style.backgroundColor = "";
+        }, 1000);
+    });
+});
